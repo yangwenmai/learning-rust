700da7e32991f3d9e53a04f52e0be2d2abdcb97a
yangwenmai <yangwen.yw@gmail.com>
2019-04-22 09:52:47 +0800

    feat(tutorial): add rand gen number

--- tutorial/guessing_game/src/main.rs
+++ tutorial/guessing_game/src/main.rs
@@ -1,19 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
-    
     .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
-
-let x = 5;
-let y=10;
-    println!("x = {} and y = {}", x, y)
 }
