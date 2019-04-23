99777af652d8672633169b6736cd0ba7422d70e5
yangwenmai <yangwen.yw@gmail.com>
2019-04-23 08:48:06 +0800

    feat(tutorial): add loop check rand number

--- tutorial/guessing_game/src/main.rs
+++ tutorial/guessing_game/src/main.rs
@@ -1,18 +1,35 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("Please input your guess.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num)=>num,
+            Err(_) => continue,
+        };
 
-    let mut guess = String::new();
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal  => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+
+
 }
