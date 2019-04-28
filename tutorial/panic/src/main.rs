1e1cd435e8f3f46fb5022a7dbf933228af3a55fe
yangwenmai <yangwen.yw@gmail.com>
2019-04-28 22:19:31 +0800

    feat(panic): add panic test

---
+++ tutorial/panic/src/main.rs
@@ -0,0 +1,22 @@
+use std::net::IpAddr;
+
+fn main() {
+    let _home: IpAddr = "127.0.0.1".parse().unwrap();
+}
+
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
