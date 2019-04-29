fc7f7a685b4b5f4930a01cc3340fb936b932eb25
yangwenmai <yangwen.yw@gmail.com>
2019-04-30 07:07:52 +0800

    feat(search): impl search by tdd

--- tutorial/minigrep/src/lib.rs
+++ tutorial/minigrep/src/lib.rs
@@ -19,6 +19,38 @@ impl Config{
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// TDD develop
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        )
+    }
 }
